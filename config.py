a40ded890217ac9f3c47a970dce706037bb7388e
Sri Bharath Sirusolla <94528077+Nitr025@users.noreply.github.com>
2023-04-15 18:21:45 +0530

    Update config.py
    commented few lines

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 class Config(object):
     SECRET_KEY = 'sasds9wn'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://", "postgresql://", 1)
+#     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+#         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+#             "postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     CORS_HEADERS = 'Content-Type'
